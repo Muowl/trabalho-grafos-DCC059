80dbebf6376198abbd5d51a4009c7446077c834b
Luizws <cbeluiz@gmail.com>
2025-01-08 18:16:58 -0300

    Implementação de funções em grafo_matriz.cpp
    Implementadas as funções get_ordem, eh_direcionado, aresta_ponderada, eh_arvore, possui_articulação e possui_ponte

--- grafo_matriz.cpp
+++ grafo_matriz.cpp
@@ -46,11 +46,11 @@ int GrafoMatriz::n_conexo() const{
     bool* visitado = new bool[ordem]();
     int componentes = 0;
     //busca em profundidade
-    auto bep = [&](int v, auto& bep_refs) -> void {
+    auto dfs = [&](int v, auto& dfs_refs) -> void {
         visitado[v] = true;
         for(int u = 0; u < ordem; u++){
             if(matriz[v][u] != 0 && !visitado[u]){
-                bep_refs(u, bep_refs);
+                dfs_refs(u, dfs_refs);
             }
         }
     };
@@ -58,7 +58,7 @@ int GrafoMatriz::n_conexo() const{
     for(int i = 0; i < ordem; i++){
         if(!visitado[i]){
             ++componentes;
-            bep(i, bep);
+            dfs(i, dfs);
         }
     }
     delete[] visitado;
@@ -74,23 +74,37 @@ int GrafoMatriz::get_grau(int vertice) const{
 }
 
 int GrafoMatriz::get_ordem() const{
-    //implementar
-    return ordem;
+    int n = 0;
+    while(matriz[n] != nullptr){
+        n++;
+    }
+    return n;
 }
 
 bool GrafoMatriz::eh_direcionado() const{
-    //implementar
-    return direcionado;
+    for(int i = 0; i < ordem; i++){
+        for(int j = 0; j < ordem; j++){
+            if (matriz[i][j] != matriz[j][i]){
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
 bool GrafoMatriz::vertice_ponderado() const{
-    //implementar
     return vertices_ponderados;
 }
 
 bool GrafoMatriz::aresta_ponderada() const{
-    //implementar
-    return arestas_ponderadas;
+    for(int i = 0; i < ordem; i++){
+        for(int j = 0; j < ordem; j++){
+            if(matriz[i][j] != 0 && matriz[i][j] != 1){
+                return true; // peso != 1 ponderadas
+            }
+        }
+    }
+    return false;
 }
 
 bool GrafoMatriz::eh_completo() const{
@@ -105,17 +119,131 @@ bool GrafoMatriz::eh_completo() const{
 }
 
 bool GrafoMatriz::eh_arvore() const{
-    //implementar
-    return false;
+    // função auxiliar para a busca em profundidade
+    auto dfs = [&](int v, bool* visitado, int pai, auto& dfs_ref)->bool {
+        visitado[v] = true;
+        for(int u = 0; u < ordem; u++){
+            if(matriz[v][u] != 0){
+                if(!visitado[u]){
+                    if(!dfs_ref(u, visitado, v, dfs_ref)){
+                        return false; //ciclo
+                    }
+                }else if(u != pai){
+                    return false;
+                }
+            }
+        }
+        return true;
+    };
+    //verificar se é conexo
+    bool* visitado = new bool[ordem]();
+    if(!dfs(0, visitado, -1, dfs)){
+        delete[] visitado;
+        return false; //ciclo
+    }
+    //verificar vertices visitados
+    for(int i = 0; i < ordem; i++){
+        if(!visitado[i]){
+            delete[] visitado;
+            return false; // desconexo
+        }
+    }
+
+    delete[] visitado;
+
+    int arestas = 0;
+    for(int i = 0; i < ordem; i++){
+        for(int j = i; j < ordem; j++){ 
+            if(matriz[i][j] != 0){
+                arestas++;
+            }
+        }
+    }
+    return arestas == ordem-1;
 }
 
 bool GrafoMatriz::possui_articulacao() const{
-    //implementar
+    // verifica componentes conexas com busca em profundidade
+    auto contar_componentes = [&](bool* ignorado) -> int {
+        bool* visitado =  new bool[ordem]();
+        int componentes = 0;
+        auto dfs = [&](int v, auto& dfs_ref) -> void {
+            visitado[v] = true;
+            for(int u = 0; u < ordem; u++){
+                if(!ignorado[u] && matriz[v][u] != 0 && !visitado[u]){
+                    dfs_ref(u,dfs_ref);
+                }
+            }
+        };
+        for(int i = 0; i < ordem; i++){
+            if(!visitado[i] && !ignorado[i]){
+                componentes++;
+                dfs(i,dfs);
+            }
+        }
+        delete[] visitado;
+        return componentes;
+    };
+    //numero componentes conexas inicialmente
+    bool* ignorado = new bool[ordem]();
+    int componentes_iniciais = contar_componentes(ignorado);
+
+    //verifica vertices como articulações
+    for(int i = 0; i < ordem; i++){
+        ignorado[i] = true;
+        int componentes_novas = contar_componentes(ignorado); //se aumenta ao ignorar i, entao i é articulação
+        ignorado[i] = false;
+        if(componentes_novas > componentes_iniciais){
+            delete[] ignorado;
+            return true;
+        }
+    }
+    delete[] ignorado;
     return false;
 }
 
 bool GrafoMatriz::possui_ponte() const{
-    //implementar
+    //contar componentes conexas com busca em profundidade
+    auto contar_componentes = [&]() -> int {
+        bool* visitado = new bool[ordem]();
+        int componentes = 0;
+        auto dfs = [&](int v, auto& dfs_ref) -> void {
+            visitado[v] = true;
+            for(int u = 0; u < ordem; u++){
+                if(matriz[v][u] != 0 && !visitado[u]){
+                    dfs_ref(u, dfs_ref);
+                }
+            }
+        };
+        for(int i = 0; i < ordem; i++){
+            if(!visitado[i]){
+                componentes++;
+                dfs(i, dfs);
+            }
+        }
+        delete[] visitado;
+        return componentes;
+    };
+    //numero inicial de componentes conexas
+    int componentes_iniciais = contar_componentes();
+    //testando arestas como possiveis pontes
+    for(int u = 0; u < ordem; u++){
+        if(matriz[u][v] != 0){ //aresta entre u e v
+            int peso_original = matriz[u][v];
+            matriz[u][v] = 0; //remover temp a aresta u,v
+            if(!direcionado){
+                matriz[u][v] = 0;
+            }
+            int componentes_novos = contar_componentes();
+            matriz[u][v] = peso_original;
+            if(!direcionado){
+                matriz[v][u] = peso_original;
+            }
+            if(componentes_novos > componentes_iniciais){ // se aumentar tem ponte
+                return true;
+            }
+        }
+    }
     return false;
 }
 
